bd07265998f7608dc9933478f3b54d16f41715cd
Lucaas <contatolucasribeirosoares@gmail.com>
2025-03-22 17:49:50 +0000

    Finalmente consegui fazer esse caraio.

--- unidade1/aula4/src/main.rs
+++ unidade1/aula4/src/main.rs
@@ -1,16 +1,3 @@
-fn main () {
-    let x = 10;
-    let y = x;
-    println("Valor de x {} Valor de y {}", x, y);
-
-    let mut z = 20;
-    {
-
-        let w = &mut z;
-        *w +=  5;
-
-        println("Novo valor de z: {}", z);
-    }
-
-
+fn main() {
+    println!("Hello, world!");
 }
